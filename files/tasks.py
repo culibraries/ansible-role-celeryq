9b96ddc0ea7f0eda0c7c7254454467df3a47693c
Mark Stacy <mbstacy@gmail.com>
2018-08-18 14:31:02 -0600

    ansible role to create celery queue

---
+++ files/tasks.py
@@ -0,0 +1,12 @@
+from celery.task import task
+
+
+#Example task
+@task()
+def add(x, y):
+    """ Example task that adds two numbers or strings
+        args: x and y
+        return addition or concatination of strings
+    """
+    result = x + y
+    return result
